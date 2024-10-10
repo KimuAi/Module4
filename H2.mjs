d69a04d522f6c7dff49ccc4e8b87679e604131dd
Kimberley Thill <ThillKimberleyPro@gmail.com>
2024-10-10 11:06:26 +0200

    H2

--- H2.mjs
+++ H2.mjs
@@ -1,9 +1,44 @@
 import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-const userInput = readline.createInterface({input, output});
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
 
-let keuze = await userInput.question(("kies uit rock, paper and scissors: "))
-let choices = ['rock', 'paper', 'scissors'];
-let computerChoice = choices[Math.floor(Math.random()*choices.length)];
-console.log(computerChoice)
+let winningCountComputer = 0; 
+let winningCountUser = 0;
 
+do {
+    let userChoice = await userInput.question("Kies uit rock, paper en scissors: ");
+    let choices = ['rock', 'paper', 'scissors'];
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    console.log(computerChoice) 
+
+    if (userChoice === computerChoice){
+        console.log("Draw!");
+
+    }else if (userChoice === "scissors" && computerChoice === "paper"){
+        console.log("User won!");
+        winningCountUser++;
+    }else if(userChoice === "paper" && computerChoice === "scissors"){
+        console.log("Computer won!");
+        winningCountComputer++;
+
+    }else if(userChoice === "paper" && computerChoice === "rock"){
+        console.log("User won!");
+        winningCountUser++;
+    }else if (userChoice === "rock" && computerChoice === "paper"){
+        console.log("Computer won!");
+        winningCountComputer++;
+
+    }else if(userChoice === "rock" && computerChoice === "scissors"){
+        console.log("User won!");
+        winningCountUser++;
+    }else if(userChoice === "scissors" && computerChoice === "rock"){
+        console.log("Computer won!");
+        winningCountComputer++;
+
+    }else{
+        console.log("Ongeldige keuze, probeer het opnieuw.");
+    }
+
+} while (winningCountComputer < 3 && winningCountUser < 3);
+
+console.log("Eindstand: User" + winningCountUser + "- Computer " +  winningCountComputer );
